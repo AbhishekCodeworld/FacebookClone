9a06d85f93bcf87a5c5da25c013a943249ce59b7
Abhishek Bajpai <abajpai@yext.com>
2023-09-03 19:07:57 +0530

    Added different Routes in the Application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+import Home from "./pages/home";
 function App() {
-  return <div>welcome to frontend</div>;
+  return <div>
+    <Routes>
+      <Route path="/login" element={<Login/>} exact/>
+      <Route path="/profile" element={<Profile/>} exact/>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  </div>;
 }
 
 export default App;
